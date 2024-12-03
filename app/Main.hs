d88ab39dcb8f1d4b88193d8ad3844e08fb8710a5
Brad Hermens <brad.hermens@gmail.com>
2024-12-02 21:40:52 -0800

    chore: some progress on part 2 day 1

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,17 @@
 module Main where
 
-import Day1 (dayOnePartOne)
+import Day1 (dayOnePartOne, dayOnePartTwo)
+
+yeet :: (Show a) => String -> a -> String
+yeet str other = str ++ " " ++ show other
 
 
 main :: IO ()
 main = do
-  x <- dayOnePartOne "input/day_one_part_one.txt"
-  print x
+  d1 <- dayOnePartOne "input/day_one_part_one.txt"
+  print (yeet "Day 1, Part 1 Result:" d1)
+  d1_2 <- dayOnePartTwo "input/day_one_part_two.txt"
+  print (yeet "Day 1, Part 2 Result:" d1_2)
+
 
 
--- src/Day1.hs
+++ src/Day1.hs
@@ -1,7 +1,10 @@
-module Day1 where
+{-# LANGUAGE TupleSections #-}
+
+module Day1 (dayOnePartOne, dayOnePartTwo) where
 
 import Data.List (sort)
 import FileRead (getWordLines)
+import Data.Map (fromListWith, Map)
 
 space :: [Integer] -> [Integer] -> [Integer]
 space x y = [abv i | i <- zip (sort x) (sort y)]
@@ -9,8 +12,8 @@ space x y = [abv i | i <- zip (sort x) (sort y)]
 abv :: (Integer, Integer) -> Integer
 abv z = abs (uncurry (-) z)
 
-total :: [Integer] -> [Integer] -> Integer
-total x y = sum (space x y)
+totalDistance :: [Integer] -> [Integer] -> Integer
+totalDistance x y = sum (space x y)
 
 wordLineToIntegers :: [String] -> [Integer]
 wordLineToIntegers x = [read i :: Integer | i <- x]
@@ -19,12 +22,25 @@ wordLinesToIntegers :: [[String]] -> [[Integer]]
 wordLinesToIntegers x = [wordLineToIntegers i | i <- x]
 
 toPairs :: [[Integer]] -> [(Integer, Integer)]
-toPairs input = [(x!!0, x!!1) | x <- input]
+toPairs input = [(head x, x!!1) | x <- input]
 
-dayOnePartOne :: String -> IO Integer
-dayOnePartOne inpStr = do
+toListInts :: String -> IO ([Integer], [Integer])
+toListInts inpStr = do
   lns <- getWordLines inpStr
   let ints = wordLinesToIntegers lns
   let pairs =  toPairs ints
-  let lsts =  unzip pairs
-  return (uncurry total lsts)
+  return (unzip pairs)
+
+
+dayOnePartOne :: String -> IO Integer
+dayOnePartOne inpStr = do
+  lsts <- toListInts inpStr
+  return (uncurry totalDistance lsts)
+
+countInts :: [Integer] -> Map Integer Integer
+countInts x = fromListWith (+) ( map (, 1) x)
+
+dayOnePartTwo :: String -> IO Map Integer Integer
+dayOnePartTwo inpStr = do
+  lsts <- toListInts inpStr
+  return (countInts (snd lsts))
